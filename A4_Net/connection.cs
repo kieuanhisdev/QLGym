af3a2af2daf4b90441e83e3f451c8c627c76031b
ineedhoma <147464691+ineedhoma@users.noreply.github.com>
2024-03-10 13:06:57 +0700

    Add files via upload

---
+++ A4_Net/connection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace A4_Net
+{
+    internal class connection
+    {
+        public static string chuoiketnoi = @"Data Source=DESKTOP-8IR3320\SQLEXPRESS;Initial Catalog=QuanLyPhongGym;Integrated Security=True";
+        public static SqlConnection GetSqlConnection() {
+            return new SqlConnection(chuoiketnoi);
+        }
+    }
+}
